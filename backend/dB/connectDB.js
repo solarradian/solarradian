997867473e80dd90c5a5be6772b57f3db7f79dd3
mohasim328 <mohasim3283@gmail.com>
2025-09-26 15:20:13 +0530

    file name

--- backend/dB/connectDB.js
+++ backend/dB/connectDB.js
@@ -1,33 +1,4 @@
-// import mongoose from 'mongoose'
-// import dotenv from 'dotenv'
 
-// dotenv.config()
-
-// if(!process.env.MONGODB_URI){
-
-//     throw new Error(
-//         "Please provide the MONGODB_URI  oin the .env file"
-//     )
-    
-// }
-
-
-//  const connectDB = async () =>{
-
-//     try {
-        
-//      const conn = await mongoose.connect(process.env.MONGODB_URI);
-
-//      console.log(`MongoDB Connected: , ${conn.connection.host}`);
-//     } catch (error) {
-//         console.log("Error Connection to MongoDB" , error.message);
-//         process.exit(1);
-//     }
-// }
-
-
-// export default connectDB;
- 
 
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
